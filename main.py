fcdc22e2aac2b20ae1adb3bcea2f9b25dbdcb686
Aoshen Zhou <aosh@canva.com>
2022-08-22 17:56:36 +1000

    working draft and OOP

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import logging
-import functions.texttospeech as tts
-import movie as movie
+import utils as utils
+from utils import Movie
 
 
 logging.basicConfig(
@@ -9,29 +9,39 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-working_folder = 'test'
-
-
 # text to speech
-url = 'https://www.reddit.com/r/AskReddit/comments/w68jfd/what_video_game_do_you_consider_a_masterpiece/'
+url = "https://www.reddit.com/r/AskReddit/comments/w68jfd/what_video_game_do_you_consider_a_masterpiece/"
 
-title = ['What video game do you consider a masterpiece?']
+title = ["What video game do you consider a masterpiece?"]
 
 text = [
-    'Portal 2',
-    'Age of empires 2',
-    'Original Halo. Felt like you were in a movie',
-    'Man i miss Valve... Their games were designed in a way many developers could only dream of, they were revolutionary in almost everything they laid their hands on.',
-    'Roller Coaster Tycoon 2',
-    'Zelda Ocarina of time',
-    'Original bioshock',
-    'Half-Life (1998)'
+    "Portal 2",
+    "Age of empires 2",
+    "Original Halo. Felt like you were in a movie",
+    "Man i miss Valve... Their games were designed in a way many developers could only dream of, they were revolutionary in almost everything they laid their hands on.",
+    "Roller Coaster Tycoon 2",
+    "Zelda Ocarina of time",
+    "Original bioshock",
+    "Half-Life (1998)",
 ]
 
 texts = title + text
 
-tts.tts(texts, working_folder)
+
+def main():
+
+    movie = Movie("test")
+
+    movie.tts(texts)
+    movie.append_audio()
+
+    print(movie.audio_duration)
+    print(movie.audio_total_duration)
+
+    movie.create_movie()
+    movie.output_movie()
 
 
+if __name__ == "__main__":
 
-# movie.movie()
+    main()
---
+++ utils.py
@@ -0,0 +1,122 @@
+import logging
+import pathlib
+import random
+from gtts import gTTS
+
+from moviepy.editor import (
+    CompositeVideoClip,
+    TextClip,
+    VideoFileClip,
+    concatenate_videoclips,
+    ImageClip,
+    ImageSequenceClip,
+    AudioFileClip,
+    CompositeAudioClip,
+)
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
+
+class Movie:
+    def __init__(self, folder: str):
+        self.folder = folder
+
+    def tts(self, text: list):
+        """
+        Create text-to-speech audio files for each iteration in list.
+        """
+        language = "en"
+        domain = "com"
+        output_folder = f"files/{self.folder}/audio"
+
+        logging.info(f"Creating {len(text)} audio clips.")
+
+        for i in range(len(text)):
+            audio = gTTS(text=text[i], tld=domain, lang=language, slow=False)
+
+            output_file = f"{output_folder}/audio_{i}.mp3"
+            audio.save(output_file)
+
+        logging.info(f"Audio clips saved in {output_folder}")
+
+        self.num_audio = len(text)
+
+    def append_audio(self):
+        """Append all text to speech audio and record duration of each clip."""
+
+        logging.info("Appending audio clips.")
+
+        audio_files = [
+            f"files/{self.folder}/audio/audio_{i}.mp3" for i in range(self.num_audio)
+        ]
+
+        audio_duration = {}
+        audio_sound = []
+        for i in range(len(audio_files)):
+
+            if i == 0:
+                start = 0
+            else:
+                start = sum(audio_duration.values())
+
+            audio_i = AudioFileClip(audio_files[i]).set_start(start)
+
+            audio_duration[i] = audio_i.duration
+            audio_sound.append(audio_i)
+
+        self.audio = CompositeAudioClip(audio_sound)
+        self.audio_duration = audio_duration
+        self.audio_total_duration = sum(self.audio_duration.values())
+
+    def create_movie(self):
+        """Overlay text screenshots on background video."""
+
+        logging.info("Overlaying text screenshots on background video.")
+
+        background = VideoFileClip(
+            f"files/{self.folder}/city.mp4", audio=False
+        ).subclip(0, self.audio_total_duration)
+
+        files = [f"files/{self.folder}/img/screen_{i}.png" for i in range(9)]
+
+        text_clips = []
+        start_time = 0
+        for f in range(len(files)):
+
+            clip_duration = self.audio_total_duration - start_time
+            x_pos = random.uniform(0.1, 0.6)
+            y_pos = random.uniform(0.1, 0.7)
+
+            img = (
+                ImageClip(files[f])
+                .set_start(start_time)
+                .set_duration(clip_duration)
+                .resize(0.5)
+                .set_position((x_pos, y_pos), "relative")
+            )
+
+            start_time = start_time + self.audio_duration[f]
+
+            text_clips.append(img)
+
+        movie = CompositeVideoClip([background] + text_clips)
+        self.movie = movie.set_audio(self.audio)
+
+    def output_movie(self):
+        """Output movie."""
+
+        logging.info(f"Outputting movie.")
+
+        self.movie.write_videofile(
+            f"files/{self.folder}/movie.mp4",
+            codec="libx264",
+            audio_codec="aac",
+            temp_audiofile="temp-audio.m4a",
+            remove_temp=True,
+        )
+
+        self.movie.close()
+        self.audio.close()
